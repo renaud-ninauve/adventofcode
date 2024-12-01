38646908dad67f20b0909f09097c77b3651da36e
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-12-01 14:17:50 +0100

    day 1 part 2

--- src/main/java/fr/ninauve/renaud/adventofcode/year2024/day01/Pair.java
+++
@@ -1,8 +0,0 @@
-package fr.ninauve.renaud.adventofcode.year2024.day01;
-
-public record Pair (long left, long right) {
-
-    public long distance() {
-        return Math.abs(right - left);
-    }
-}
---
+++ src/main/java/fr/ninauve/renaud/adventofcode/year2024/day01/Part02.java
@@ -0,0 +1,43 @@
+package fr.ninauve.renaud.adventofcode.year2024.day01;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Part02 {
+
+    public static void main(String... args) throws Exception {
+        List<String> lines = Files.readAllLines(Path.of(Part02.class
+                .getResource("/fr/ninauve/renaud/adventofcode/year2024/day01/input.txt").toURI()));
+
+        List<Long> left = new ArrayList<>();
+        List<Long> right = new ArrayList<>();
+
+        for (String line : lines) {
+            String[] splitted = line.split("\\s+");
+            if (splitted.length < 2) {
+                continue;
+            }
+            left.add(Long.parseLong(splitted[0]));
+            right.add(Long.parseLong(splitted[1]));
+        }
+
+        final Map<Long, Long> rightFreq = right.stream()
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long sum = 0;
+        for (int i = 0; i < left.size(); i++) {
+            Long leftValue = left.get(i);
+            Long freq = rightFreq.getOrDefault(leftValue, 0L);
+            long similarity = leftValue * freq;
+            sum += similarity;
+        }
+
+        System.out.println(sum);
+    }
+}
