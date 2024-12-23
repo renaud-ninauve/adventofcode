ca09b4cdb6208582e8ade17273455d6744094630
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-12-23 09:44:46 +0100

    2024 day 17 part 2 wip

--- src/main/java/fr/ninauve/renaud/adventofcode/year2024/day17/Part02.java
+++ src/main/java/fr/ninauve/renaud/adventofcode/year2024/day17/Part02.java
@@ -27,14 +27,27 @@ public class Part02 {
         for (int i = 0; i < program.size(); i++) {
             final BigWord data = program.get(i);
             a = find(cpu, i, a, data);
-            System.out.println("[" + i + "] " + Integer.toOctalString(a));
+            System.out.println("[" + i + "] " + Integer.toBinaryString(a));
         }
         return a;
     }
 
     private static int find(Cpu cpu, int index, int currentA, BigWord expected) {
-        for (int i = 0; i < 1000; i++) {
-            int a = Integer.rotateLeft(i, index * 3) + currentA;
+        int distance = index * 3;
+        final int mask = Integer.rotateLeft(7, distance);
+        final int maskA = Integer.MAX_VALUE ^ mask;
+        //final int maskedA = currentA & maskA; // bits corresponding to index are 0
+        int maskI = 0;
+        for(int i= 0; i<index; i++) {
+            final int maskIndex = Integer.rotateLeft(7, i * 3);
+            maskI = maskI | maskIndex;
+        }
+        maskI = ~maskI;
+        final int maskedA = currentA & (~maskI);
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            int maskedI = Integer.rotateLeft(i, distance) & maskI; // bits not corresponding to index are 1
+
+            int a = maskedI | maskedA;
             Cpu newCpu = cpu.registerA(BigWord.valueOf(a));
             List<BigWord> output = Part02.execute(newCpu);
             if (output.size() > index && output.get(index).equals(expected)) {
