7f82bba478d17c24df60ba4097372a0aa7199728
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-12-30 18:17:07 +0100

    suffix tree print

--- src/test/java/fr/ninauve/renaud/adventofcode/common/SuffixTreeTest.java
+++ src/test/java/fr/ninauve/renaud/adventofcode/common/SuffixTreeTest.java
@@ -53,81 +53,46 @@ class SuffixTreeTest {
     void from_empty() {
         SuffixTree actual = SuffixTree.from("");
 
-        SuffixTree.Node expectedRoot = rootNode();
-        expectedRoot.append("" + SuffixTree.ENDING_CHAR, leafNode(0));
-        assertThat(actual).isEqualTo(new SuffixTree(
-                SuffixTree.ENDING_CHAR,
-                expectedRoot
-        ));
+        assertThat(actual.print()).isEqualTo("""
+                $0
+                """);
     }
 
     @Test
     void from_one_char() {
         SuffixTree actual = SuffixTree.from("a");
 
-        SuffixTree.Node expectedRoot = rootNode();
-        expectedRoot.append("" + SuffixTree.ENDING_CHAR, leafNode(1));
-        SuffixTree.Node expectedFirstChild = intermediateNode();
-        expectedRoot.append("a", expectedFirstChild);
-
-        SuffixTree.Node expectedLeaf = leafNode(0);
-        expectedFirstChild.append("" + SuffixTree.ENDING_CHAR, expectedLeaf);
-
-        assertThat(actual).isEqualTo(new SuffixTree(
-                SuffixTree.ENDING_CHAR,
-                expectedRoot
-        ));
+        assertThat(actual.print()).isEqualTo("""
+                a
+                ..$0
+                $1
+                """);
     }
 
     @Test
     void from_2_different_chars() {
         SuffixTree actual = SuffixTree.from("ab");
 
-        SuffixTree.Node root = rootNode();
-
-        // ending -> 2
-        root.append(ENDING_LABEL, leafNode(2));
-
-        // b -> 1
-        SuffixTree.Node b = intermediateNode();
-        root.append("b", b);
-        b.append(ENDING_LABEL, leafNode(1));
-
-        // ab -> 0
-        SuffixTree.Node a = intermediateNode();
-        root.append("a", a);
-        SuffixTree.Node a_b = intermediateNode();
-        a.append("b", a_b);
-        a_b.append(ENDING_LABEL, leafNode(0));
-
-        assertThat(actual).isEqualTo(new SuffixTree(
-                SuffixTree.ENDING_CHAR,
-                root
-        ));
+        assertThat(actual.print()).isEqualTo("""
+                a
+                ..b
+                ....$0
+                b
+                ..$1
+                $2
+                """);
     }
 
     @Test
     void from_2_same_chars() {
         SuffixTree actual = SuffixTree.from("aa");
 
-        SuffixTree.Node root = rootNode();
-
-        // ending -> 2
-        root.append(ENDING_LABEL, leafNode(2));
-
-        // a -> 1
-        SuffixTree.Node a = intermediateNode();
-        root.append("a", a);
-        a.append(ENDING_LABEL, leafNode(1));
-
-        // aa -> 0
-        SuffixTree.Node a_a = intermediateNode();
-        a.append("a", a_a);
-        a_a.append(ENDING_LABEL, leafNode(0));
-
-        assertThat(actual).isEqualTo(new SuffixTree(
-                SuffixTree.ENDING_CHAR,
-                root
-        ));
+        assertThat(actual.print()).isEqualTo("""
+                a
+                ..a
+                ....$0
+                ..$1
+                $2
+                """);
     }
 }
