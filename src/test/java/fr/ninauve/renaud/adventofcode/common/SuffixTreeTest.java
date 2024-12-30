f89d77d8d053fa414299472065a2f32873747dd6
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-12-30 17:26:32 +0100

    suffix tree 'ab'

--- src/test/java/fr/ninauve/renaud/adventofcode/common/SuffixTreeTest.java
+++ src/test/java/fr/ninauve/renaud/adventofcode/common/SuffixTreeTest.java
@@ -6,6 +6,7 @@ import static fr.ninauve.renaud.adventofcode.common.SuffixTree.Node.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SuffixTreeTest {
+    static final String ENDING_LABEL = "" + SuffixTree.ENDING_CHAR;
 
     @Test
     void from_empty() {
@@ -34,6 +35,33 @@ class SuffixTreeTest {
         assertThat(actual).isEqualTo(new SuffixTree(
                 SuffixTree.ENDING_CHAR,
                 expectedRoot
+        ));
+    }
+
+    @Test
+    void from_2_different_chars() {
+        SuffixTree actual = SuffixTree.from("ab");
+
+        SuffixTree.Node root = rootNode();
+
+        // ending -> 2
+        root.append(ENDING_LABEL, leafNode(2));
+
+        // b -> 1
+        SuffixTree.Node b = intermediateNode();
+        root.append("b", b);
+        b.append(ENDING_LABEL, leafNode(1));
+
+        // ab -> 0
+        SuffixTree.Node a = intermediateNode();
+        root.append("a", a);
+        SuffixTree.Node a_b = intermediateNode();
+        a.append("b", a_b);
+        a_b.append(ENDING_LABEL, leafNode(0));
+
+        assertThat(actual).isEqualTo(new SuffixTree(
+                SuffixTree.ENDING_CHAR,
+                root
         ));
     }
 }
